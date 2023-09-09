b32873780659f767b690e581ab8276eaf100fb90
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-09-09 19:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let found = false;
+	for (let i = 0; i < str.length; i++) {
+		if(str.charAt(i)!=str.charAt(i+1)){
+			found = true;
+			return str.charAt(i)
+		}else{
+			i++;
+		}
+	}
+	
+	if(!found)return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
