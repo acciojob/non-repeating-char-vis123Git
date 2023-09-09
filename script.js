6b1f5403711ce06656284ec0736fd72c1fd92ba7
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-09-09 19:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 function firstNonRepeatedChar(str) {
-	let found = false;
 	for (let i = 0; i < str.length; i++) {
-		if(str.charAt(i)!=str.charAt(i+1)){
-			found = true;
-			return str.charAt(i)
-		}else{
-			i++;
+		let j=i;
+		let found = false;
+		while (j<str.length && !found) {
+			if(str.charAt(i) == str.charAt(j)){
+				found = true;
+			}
 		}
+
+		if(!found)return str.charAt(i);
+		else return null;
+		// if(str.charAt(i)!=str.charAt(i+1)){
+		// 	found = true;
+		// 	return str.charAt(i)
+		// }else{
+		// 	i++;
+		// }
 	}
+	
 	
 	if(!found)return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
